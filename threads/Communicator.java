a229fc5c4001166ae84f1a3f1d85a08fbeb6656b
munsta2 <79557328+munsta2@users.noreply.github.com>
2022-03-07 22:53:42 -0500

    test commit for task 4

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -11,16 +11,23 @@ import nachos.machine.*;
  */
 public class Communicator {
     
-    Int Message;
-    Boolean wordSpoken;
-    Condition2 speaker;
-    Condition2 listener;
-    Lock lock;
-
+    private static final char CommunicatorTestChar = 'c';
+    private int message;
+    private Boolean wordSpoken;
+    private Condition speaker;
+    private Condition listener;
+    private Lock lock;
+    private int numListners;
     /**
      * Allocate a new communicator.
      */
     public Communicator() {
+
+        lock = new Lock();
+        speaker = new Condition(lock);
+        listener = new Condition(lock);
+        wordSpoken = true;
+        numListners = 0;
     }
 
     /**
@@ -34,6 +41,17 @@ public class Communicator {
      * @param	word	the integer to transfer.
      */
     public void speak(int word) {
+        lock.acquire();
+
+        while(numListners == 0 || (!wordSpoken)) {
+            speaker.sleep();
+    
+        }
+
+        wordSpoken = false;
+        message = word;
+        listener.wake();
+        lock.release();
     }
 
     /**
@@ -43,6 +61,87 @@ public class Communicator {
      * @return	the integer transferred.
      */    
     public int listen() {
-	return 0;
+        lock.acquire();
+
+        numListners++;
+        speaker.wake();
+
+        listener.sleep();
+
+        int wordTransfer = message;
+        wordSpoken = true;
+
+        numListners--;
+        lock.release();
+        return wordTransfer;
+
+    }
+
+    public static void selfTest(){
+        Lib.debug(CommunicatorTestChar, "Comuincator test case");
+
+		basicTest();
+        testTest();
+    
+    }
+
+    public static void basicTest() {
+        final Communicator cTest = new Communicator();
+
+		//Test case 1
+		KThread test1a = new KThread(new Runnable(){
+			public void run(){
+				Lib.debug(CommunicatorTestChar, "Test Case 1: basic test one speaker and lisitner.");
+				Lib.debug(CommunicatorTestChar, "Sending message 15");
+				cTest.speak(15);
+			}
+		});
+
+		test1a.fork();
+
+		KThread test1b = new KThread(new Runnable(){
+			public void run(){
+				int result = cTest.listen();
+				Lib.debug(CommunicatorTestChar, "" + result);
+				if(result == 15)
+					Lib.debug(CommunicatorTestChar, "Test 1 Successful! Message " + result + " received.\n");
+				else
+					Lib.debug(CommunicatorTestChar, "Test 1 Failed.\n");
+			}
+		});
+
+		test1b.fork();
+		test1b.join();
+		test1a.join();
+    }
+    
+    public static void testTest(){
+        final Communicator cTest = new Communicator();
+      
+		//Test case 1
+		KThread test1a = new KThread(new Runnable(){
+			public void run(){
+                System.out.println("Test Case 1: basic test one speaker and lisitner.");
+                System.out.println("Sending message 15");
+				cTest.speak(15);
+			}
+		});
+
+		test1a.fork();
+
+		KThread test1b = new KThread(new Runnable(){
+			public void run(){
+				int result = cTest.listen();
+                System.out.println(result);
+				if(result == 15)
+                    System.out.println("Test 1 Successful! Message " + result + " received.\n");
+				else
+                    System.out.println("Test 1 Failed.\n");
+			}
+		});
+
+		test1b.fork();
+		test1b.join();
+		test1a.join();
     }
 }
