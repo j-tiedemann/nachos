aa390fd0b07ab1fc75a50d20de74ddbe6bb17295
Tidomann <tidomann@gmail.com>
2022-03-08 15:20:49 -0500

    Update Alarm.java
    Implemented designed methods of timerInterrupt and waitUntil using PriorityQueue object and custom nested inner class object waitThread.

--- threads/Alarm.java
+++ threads/Alarm.java
@@ -2,6 +2,8 @@ package nachos.threads;
 
 import nachos.machine.*;
 
+import java.util.PriorityQueue;
+
 /**
  * Uses the hardware timer to provide preemption, and to allow threads to sleep
  * until a certain time.
@@ -27,7 +29,16 @@ public class Alarm {
      * that should be run.
      */
     public void timerInterrupt() {
-	KThread.currentThread().yield();
+
+        long currentTime = Machine.timer().getTime();
+        boolean interrupt = Machine.interrupt().disable();
+
+        while(!waitQueue.isEmpty() && (waitQueue.peek().getWakeTime() <= currentTime)){
+            waitQueue.poll().getWaitThread().ready();
+        }
+
+        Machine.interrupt().restore(interrupt)
+        KThread.yield();
     }
 
     /**
@@ -44,10 +55,60 @@ public class Alarm {
      *
      * @see	nachos.machine.Timer#getTime()
      */
-    public void waitUntil(long x) {
-	// for now, cheat just to get something working (busy waiting is bad)
-	long wakeTime = Machine.timer().getTime() + x;
-	while (wakeTime > Machine.timer().getTime())
-	    KThread.yield();
+    public void waitUntil(long waitTime) {
+        //store the interrupt status
+        boolean interrupt = Machine.interrupt().disable();
+        //calculate the time the thread needs to wake
+        long wakeTime = Machine.timer().getTime() + waitTime;
+        //use custom waitThread object to store KThread and calculated wakeTime
+        waitThread waiter = new waitThread(KThread.currentThread(), wakeTime);
+
+        waitQueue.add(waiter);
+        KThread.sleep();
+
+        Machine.interrupt().restore(interrupt);
+    }
+
+    private class waitThread implement Comparable<waitThread>{
+        //waitThread data fields
+        private long wakeTime;
+        private KThread waitThread;
+
+        //waitThread class constructor
+        public waitThread(KThread thread, long time){
+            this.waitThread = thread;
+            this.wakeTime = time;
+        }
+
+        public long getWakeTime(){
+            return wakeTime;
+        }
+
+        public Kthread getWaitThread(){
+            return waitThread;
+        }
+
+        //Comparable Interface Implementation
+        public int compareTo(waitThread otherThread){
+            if(wakeTime > other.wakeTime)
+                return 1;
+            else if(wakeTime == other.wakeTime)
+                return 0;
+            else
+                return -1;
+        }
+    }
+
+    //Priority queue object implementation using the custom object waitThread to store and sort threads according
+    //to their timeToWait
+    private PriorityQueue<waitThread> waitQueue = new PriorityQueue<waitThread>();
+
+    /**
+     * selfTest() method tests to make sure that threads are put to sleep and awoken in the proper order. The order
+     * is determined by the timeToWait rather than the order in which the threads were put to sleep.
+     */
+    private static final char AlarmTestChar = 'a';
+    public static void selftest(){
+
     }
 }
