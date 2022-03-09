50b1da69138293457f62f42759365a21545c3da7
munsta2 <79557328+munsta2@users.noreply.github.com>
2022-03-08 22:51:40 -0500

    Update Condition2.java

--- threads/Condition2.java
+++ threads/Condition2.java
@@ -7,15 +7,15 @@ import nachos.machine.Machine;
 
 public class Condition2 {
 
-  private static final char dbgC2 = 'c';
+  private static final char dbgC2 = 'f';
   private Lock conditionLock;
   private LinkedList<KThread> waitList;
   
 	public Condition2(Lock conditionLock) {
 		this.conditionLock = conditionLock;
-		Lib.debub(dbgC2, Machine.timer().getTime() + "Cnd2 start";
+		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 start");
 		this.waitList = new LinkedList<KThread>();
-		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 end";
+		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 end");
 		}
 
 	//Threads and interrupts must be suspended to maintain atomicity
@@ -23,35 +23,43 @@ public class Condition2 {
 	//sleep() method adds threads to our waitList for future use 
 		
 	public void sleep() {
-		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 sleep start";
+		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 sleep start");
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-		Lib.assertTrue(KThread.currentThread().status != statusFinished);
-		Queue.add(KThread.currentThread());
-		Machine.interrupt().disable(); 
-		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 sleep: interrupts disabled";
+		//Lib.assertTrue(KThread.currentThread().status != statusFinished);
+		
+		
+		waitList.add(KThread.currentThread());
+		boolean intSatus = Machine.interrupt().disable(); 
+		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 sleep: interrupts disabled");
 		conditionLock.release();
-		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 sleep: lock released";
+		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 sleep: lock released");
 		KThread.currentThread().sleep();
-		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 lock slept";
+		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 lock slept");
 		conditionLock.acquire();
-		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 sleep: lock acquired";
-		Machine.interrupt().enable();
-		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 sleep: interrupts enabled";
+		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 sleep: lock acquired");
+		Machine.interrupt().restore(intSatus);
+		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 sleep: interrupts enabled");
 	   }
 
 	//wake() uses a waker thread to pull a thread out of the waitList //for use
 	public void wake() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-		
-		if (!waitList.isEmpty()) {
-			Machine.interrupt().disable();
-			Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 wake: interrupts disabled";
+		boolean intSatus = Machine.interrupt().disable();
+		if (waitList.peek() != null) {
+			
+			Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 wake: interrupts disabled");
+			try {
+				((KThread)waitList.poll()).ready();
+			}
+			catch(Exception e) {
+				
+			}
+			
+			
 		}
-		KThread waker = new KThread();
-		waker = (KThread) waitList.removeFirst();
-		waker.ready();
-	Machine.interrupt().enable();
-	Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 wake: interrupts disabled";
+		
+		Machine.interrupt().restore(intSatus);
+		Lib.debug(dbgC2, Machine.timer().getTime() + "Cnd2 wake: interrupts disabled");
 		}
 	public void wakeAll() {
 			Lib.assertTrue(conditionLock.isHeldByCurrentThread());
@@ -62,10 +70,10 @@ public class Condition2 {
 	}
 	
 	
-	public void selfTest()
+	public static void selfTest()
 	{
 		final Lock sTLock = new Lock();
-		final Condition2 sTCond = new Condition2(testLock);
+		final Condition2 sTCond = new Condition2(sTLock);
 		KThread sleepThread1 = new KThread(new Runnable(){
 			public void run(){
 				sTLock.acquire();
@@ -75,12 +83,11 @@ public class Condition2 {
 				System.out.println("sleepThread1: test complete");
 				sTLock.release();
 				System.out.println("sleepThread1: Lock released.");
-	}
-		}
-		).setName("Sleep Test 1");
-			System.out.prinlnt("Forking sleepThread 1");
+		}});
+		sleepThread1.setName("Sleep Test 1");
+			System.out.println("Forking sleepThread 1");
 			sleepThread1.fork();
-			KThread sleepThread2 = new KThread runnable(){
+			KThread sleepThread2 = new KThread(new Runnable(){
 				
 				public void run() {
 					sTLock.acquire();
@@ -93,10 +100,10 @@ public class Condition2 {
 					System.out.println("sleepThread2: Lock released.");
 	
 				}
-			}
+			});
 			
-		).setName("Sleep Test 2");
-			System.out.prinlnt("Forking sleepThread 2");
+			sleepThread2.setName("Sleep Test 2");
+			System.out.println("Forking sleepThread 2");
 			sleepThread2.fork();
 				
 			KThread sleepThread3 = new KThread(new Runnable(){
@@ -113,31 +120,13 @@ public class Condition2 {
 				System.out.println("sleepThread3: Lock released.");
 					
 				}
-			}
-		).setName("Sleep Test 3");
-				System.out.prinlnt("Forking sleepThread 2");
-				sleepThread2.fork();
-				
-				KThread sleepThread3 = new KThread(new Runnable(){
-					
-							
-				public void run() {
-					sTLock.acquire();
-					System.out.println("sleepThread3 Lock acquired. Sleeping...");
-					sTCond.sleep();
-					
-					System.out.println("sleepThread3 slept");
-					System.out.println("sleepThread3: test complete");
-					sTLock.release();
-					System.out.println("sleepThread3: Lock released.");
-					
-				}
-			}
-		).setName("Wake test 1");
-				System.out.prinlnt("Forking sleepThread3");
+			});
+		
+			sleepThread3.setName("Wake test 1");
+				System.out.println("Forking sleepThread3");
 				sleepThread3.fork();
 				
-				KThread wakeThread1 = new KThread(new runnable(){
+				KThread wakeThread1 = new KThread(new Runnable(){
 					
 					public void run() {
 					sTLock.acquire();
@@ -150,12 +139,12 @@ public class Condition2 {
 					System.out.println("wakeThread1: Lock released.");
 					
 				}
-				}
-		).setName("Wake Test 2");
-			System.out.prinlnt("Forking wakeThread1");
+				});
+				wakeThread1.setName("Wake Test 2");
+			System.out.println("Forking wakeThread1");
 				wakeThread1.fork();
 				
-				KThread wakeThread2 = new KThread(new runnable(){
+				KThread wakeThread2 = new KThread(new Runnable(){
 					
 					public void run() {
 					sTLock.acquire();
@@ -167,24 +156,28 @@ public class Condition2 {
 					sTLock.release();
 					System.out.println("wakeThread2 Lock released.");
 					}
-				}
-		).setName("Wake Test 3");
-			System.out.prinlnt("Forking wakeThread1");
+				});
+				wakeThread2.setName("Wake Test 3");
+			System.out.println("Forking wakeThread1");
 			wakeThread2.fork();
 				
-			KThread wakeThread3 = new KThread(new runnable(){
+			KThread wakeThread3 = new KThread(new Runnable(){
 					
 					public void run() {
 					sTLock.acquire();
 					System.out.println("wakeThread2 Lock acquired. Waking all with no wakeable threads...");
 					sTCond.wakeAll();
-					System.out.println("No threads to wake")'
+					System.out.println("No threads to wake");
 					System.out.println("wakeThread2 test complete");
 					sTLock.release();
 					System.out.println("wakeThread2 Lock released.");
 				}
-					
-				
-					
-				
+			});
+	}
 }
+	
+
+
+				
+					
+				
