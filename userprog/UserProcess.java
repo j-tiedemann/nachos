9743b15e99fe8597cd953ab04ebb2d834233cb37
NachosGuan <99605576+NachosGuan@users.noreply.github.com>
2022-04-13 13:38:04 -0400

    Update UserProcess.java

--- userprog/UserProcess.java
+++ userprog/UserProcess.java
@@ -200,41 +200,84 @@ public class UserProcess{
 	Public int sysClose(int fileDescriptor){
 
 		if(fileDescriptor < 0 || fileDescriptor > 15)
-			Return -1; 
+			return -1; 
 
 		if(globalFileTable[fileDescriptor] == null)
-			Return -1; 
+			return -1; 
 
 		Else {
 			globalFileTable[fileDescriptor].close();
 			globalFileTable[fileDescriptor] = null;
 		}
-		Return 0; 
+		return 0; 
 	}
 	
 	Public int unlink(int nameAddress) {
 		if(nameAddress < 0)
-			Return -1;
+			return -1;
 
 		String file = readVirtualMemoryString(nameAddress, 256);
 		if(file == null)
-		Return -1;
+			return -1;
 
-		Int result = -1;
-		for(int index = 0; index < listMax; index++){
+		int result = -1;
+		for(int index = 0; index < 15; index++){
 			if(globalFileTable[index] != null && globalFileTable[index] == file){
-				Result == index;
-				Break;
+				result == index;
+				break;
 			}
 		if(result != -1)
-			Return -1;
+			return -1;
 
 		if(ThreadKernel.fileSystem.remove(file))
-			Return 0;
+			return 0;
 
-		Else 
-			Return -1; 
+		else 
+			return -1; 
 	}
+	
+	Public int Syswrite(int fileDescriptor, int bufferAddress, int count){
+	
+		if(count < 0)
+			return -1;
+
+		if(fileDescriptor < 0 || fileDescriptor > 15)
+			return -1; 
+
+		if(globalFileTable[fileDescriptor] == null;)
+			return -1;
+
+		int bytesRemaining = count; 
+		Byte [] writing = new byte[count]; 
+		int increment = 0; 
+		int written = 0; 
+		int toWrite; 
+		int total = 0;
+
+		while(bytesRemaining > 0){
+			if(bytesRemaining < pageSize){
+				increment = bytesRemaining; }
+			else {
+				increment = pageSize; }
+			
+			toWrite = readVirtualMemory(bufferAddress, buff, increment);
+			Written = writeFile.write(buff, 0, toWrite); 
+			
+
+		if(written == -1) 
+			return -1; 
+
+		if(toWrite != Written || Written != increment) 
+			return -1;
+
+		total += written; 
+		bytesRemaining -= written; 
+		bufferAddress += written; 
+		}
+
+		return total;
+	}
+
 
 
 
